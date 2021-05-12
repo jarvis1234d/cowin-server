37dab93ea4f6fa2d7099c8cd61bf7869ffb1ae4e
Stutter <indtarun5@gmail.com>
2021-05-12 16:47:38 +0530

    Fixed all the problems mentioned in the previous commit: 	All the calls to the api are asyncronous. 	Errors have been handeled (for most of the cases).
    	Created a webpage to recieve the otp from the user.
    	This system can futher be utilised to get other details.

--- index.js
+++ index.js
@@ -4,55 +4,79 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const axios = require('axios');
 const sha256 = require('js-sha256');
-//const prompt = require('prompt-sync');
+//const ejs = require('ejs');
 
 var txn_Id;
+var token;
 
 app = express();
 app.use(bodyParser.json());
+var urlencodedParser = bodyParser.urlencoded({extended: true});
+//app.set('view engine', 'ejs');
 
-async function start(){
-    confirmOTP = {
-        method: 'post',
-        url: 'https://cdn-api.co-vin.in/api/v2/auth/public/confirmOTP',
-        data:{
-            otp: String,
-            txnId: String
-        }
+confirmOTP = {
+    method: 'post',
+    url: 'https://cdn-api.co-vin.in/api/v2/auth/public/confirmOTP',
+    data:{
+        otp: String,
+        txnId: String
     }
+}
     
-    generateOTP = {
-        method: 'post',
-        url: 'https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP',
-        data: {
-            mobile: "8308873441"
-        }
+generateOTP = {
+    method: 'post',
+    url: 'https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP',
+    data: {
+    mobile: "8308873441"
     }
+}
+
+// async function getOTP(){
+
+// }
+
+async function getOTP(){
+    
     
     await axios(generateOTP)
-    .catch((error)=>{
-        console.log(error.response.data);
-        console.log(error.response.status);
-    })
     .then((res)=>{
         console.log(res.data);
         txn_Id = res.data.txnId;
+    })
+    .catch((error)=>{
+        console.log(error.response.status);
+        console.log(error.response.data);
     });
-    //console.log(response);
-    confirmOTP.data.otp = sha256("112254");
+} 
+
+async function sendOTP(){
+    
+    //confirmOTP.data.otp = sha256(confirmOTP.data.otp);
+    console.log("Recieved OTP: "+confirmOTP.data.otp);
     confirmOTP.data.txnId = txn_Id;
     
     await axios(confirmOTP)
-    .catch((error)=>{
-        console.log(error.response.data);
-        console.log(error.response.status);
-    
-    })
     .then((res)=>{
-        console.log(res.data);
+        token = res.data.token
+        console.log("TOKEN: "+token);
+    })
+    .catch((error)=>{
+        console.log(error.response.status);
+        console.log(error.response.data);
     });
 }
-start();
+getOTP();
+
+
+app.get('/', (req, res)=>{
+    res.sendFile( __dirname + '/views/otp.html');
+})
+
+app.post('/otp', urlencodedParser, (req, res)=>{
+   confirmOTP.data.otp = sha256(req.body.otp);
+   sendOTP();
+   res.redirect('/');
+})
 
 app.listen(3000, (req, res)=>{
     console.log("server started");
